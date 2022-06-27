c4ebd0057a0c5ef197db685fa0f479a7cb0b746d
Abdullahi Addow <jiincade@gmail.com>
2022-06-27 17:02:21 +0300

    small home page design.

--- lib/pages/small_home_page.dart
+++ lib/pages/small_home_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:resume/widgets/about_widget.dart';
+
+import '../models/project.dart';
+import '../providers/project_provider.dart';
+import '../services/project_service.dart';
+import '../widgets/end_drawer_widget.dart';
+import '../widgets/project_details_widget.dart';
+import '../widgets/small_app_bar_widget.dart';
 
 class SmallHomePage extends StatelessWidget {
-  const SmallHomePage({Key? key}) : super(key: key);
+  SmallHomePage({Key? key})
+      : projects = ProjectService.projects(),
+        super(key: key);
+  final List<Project> projects;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Small Home page')),
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: SmallAppBarWidget(),
+      ),
+      endDrawer: EndDrawerWidget(),
+      body: ValueListenableBuilder(
+        valueListenable: selectedIndexNotifier,
+        builder: (_, int selectedIndex, __) {
+          Navigator.maybePop(context);
+          if (selectedIndex < 0) return const AboutWidget();
+          return ProjectDetailsWidget(
+            project: projects[selectedIndex],
+          );
+        },
+      ),
     );
   }
 }
