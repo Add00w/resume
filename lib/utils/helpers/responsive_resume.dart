5fa3db3efdf34723f887bbd8c0ebbb809aa1ab1f
Abdullahi Addow <jiincade@gmail.com>
2022-06-27 16:57:31 +0300

    remove the medium page.

--- lib/utils/helpers/responsive_resume.dart
+++ lib/utils/helpers/responsive_resume.dart
@@ -1,13 +1,9 @@
 class ResponsiveResume {
-  static const largeScreenSize = 1280;
-  static const mediumScreenSize = 601;
+  static const largeScreenSize = 1181;
   static late double width;
   static void init({required double maxWidth}) {
     width = maxWidth;
   }
 
   static bool get isLargeScreen => width > largeScreenSize;
-  static bool get isMediumScreen => (width >= mediumScreenSize);
-
-  static bool get isSmallScreen => width < mediumScreenSize;
-}
+ }
