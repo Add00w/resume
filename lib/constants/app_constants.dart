1fae5bd23055c9099f470778b7da38577b351921
Abdullahi Addow <jiincade@gmail.com>
2022-06-27 16:59:31 +0300

    move fixed assets to constants.

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -2,4 +2,9 @@ class AppConstants {
   static const githubProfile = 'https://github.com/Add00w';
   static const linkedInProfile = 'https://linkedin.com/in/add0w';
   static const twitterProfile = 'https://twitter.com/Add00w';
+//assets
+  static const twitterLogo = 'assets/images/twitter.svg';
+  static const linkedInLogo = 'assets/images/linkedin.svg';
+  static const githubLogo = 'assets/images/github.svg';
+  static const me = 'assets/images/me.jpg';
 }
